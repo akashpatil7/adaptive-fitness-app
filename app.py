c4690229ad191ad213f23e6639c35f39a7b9f0bd
Akash Patil <apatil@tcd.ie>
2022-04-02 01:43:57 +0100

    Added weight goals dependency in formula

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, make_response
 from flask import json, request
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("blackbird-545eb-firebase-adminsdk-mbj63-3e346d4374.json")
+cred = credentials.Certificate("blackbird-545eb-firebase-adminsdk-mbj63-6dffc14bc3.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -21,10 +21,11 @@ def hello_world():
 def enterQuestionaireForm():
     id = request.json['email']
 
-    explicit_data.document(id).set(request.json)
+    request_body = request.json
+    #explicit_data.document(id).set(request.json)
 
     #Get the initial diet and workout plan
-    data = getInitialPlansForUser(request.json)
+    data = getInitialPlansForUser(request_body)
 
     response = app.response_class(
         response=json.dumps(data),
@@ -40,13 +41,26 @@ def enterQuestionaireForm():
 
 #Update data for user
 def getInitialPlansForUser(data):
+    id = data['email']
     bmr = 0
-    if data["gender"] is "male":
+    if data["gender"] == "male":
         bmr = 66.5 + (13.75 * data["weight"]) + (5.003 * data["height"]) - (4.676 * data["age"])
-    elif data["gender"] is "female":
+    elif data["gender"] == "female":
         bmr = 655.1 + (9.563 * data["weight"]) + (1.850 * data["height"]) - (4.676 * data["age"])
 
-    plans_by_weight_goals = food_plans_data.document(data["weight_goals"]).get().to_dict()
+    if data["activity_level_ratio"] == "sedentary":
+        bmr = bmr + 300
+    elif data["activity_level_ratio"] == "light":
+        bmr = bmr + 600
+    elif data["activity_level_ratio"] == "moderate":
+        bmr = bmr + 900
+    elif data["activity_level_ratio"] == "active":
+        bmr = bmr + 1200
+    elif data["activity_level_ratio"] == "athlete":
+        bmr = bmr + 1500
+
+    weight_goals = data["weight_goals"]
+    plans_by_weight_goals = food_plans_data.document(weight_goals).get().to_dict()
     plans_by_diet_restrictions = plans_by_weight_goals[data["dietary_restrictions"]]
 
     total_cals_in_plans = 0
@@ -56,6 +70,8 @@ def getInitialPlansForUser(data):
         total_cals_per_meal[meal] = meal["nutrition"]["cal"]
 
     if bmr < total_cals_in_plans:
+        data["current_food_plan"] = plans_by_diet_restrictions
+        explicit_data.document(id).set(data)
         return plans_by_diet_restrictions
     else:
         extra_calories_needed_per_meal = (bmr - total_cals_in_plans)/3
@@ -63,6 +79,8 @@ def getInitialPlansForUser(data):
         for meal in new_plan:
             for ingredient, val in meal["ingredients"].items():
                 val += val * (extra_calories_needed_per_meal / total_cals_per_meal[meal])
+        data["current_food_plan"] = new_plan
+        explicit_data.document(id).set(data)
         return new_plan
 
 if __name__ == "__main__":
